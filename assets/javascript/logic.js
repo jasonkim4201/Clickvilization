59b5533868e19f5682cb4e62c0e2b4696d059608
Jason Kim <jasonkim4201@gmail.com>
2019-01-20 18:24:47 -0500

    updated to have totals to be changed dynamically

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,44 +10,23 @@ var woodLimit = 5;
 var stoneLimit = 5;
 var ironLimit = 5;
 
+//add resource limits on to html page
+$("#foodLimit").html(foodLimit);
+$("#woodLimit").html(woodLimit);
+$("#stoneLimit").html(stoneLimit);
+$("#ironLimit").html(ironLimit);
 
   //on click function on basic resources
   $("#foodBtn, #woodBtn, #stoneBtn, #ironBtn").on("click", function() {   
-    /* if (this.id === "foodBtn") {
-      //check to see if limit has not been reached
-      if (foodTotal >= foodLimit) {
-        return console.log(`Food limit of ${foodLimit} has been reached.`);
-      }
-      //when specific resource is clicked then it is added to the total and updated on html page.
-      else {
-      foodTotal++;
-      console.log(`Food: ${foodTotal}`);
-      }
-    }
-    else if (this.id === "woodBtn") {
-      if (woodTotal >= woodLimit) {
-        return console.log(`Wood limit of ${woodLimit} has been reached.`);
-      } 
-      else {
-      woodTotal++;
-      console.log(`Wood: ${woodTotal}`);
-      }  
-    }
-    else if (this.id === "stoneBtn") {
-      stoneTotal++;
-      console.log(`Stone: ${stoneTotal}`);
-    }
-    else if (this.id === "ironBtn") {
-      ironTotal++;
-      console.log(`Iron: ${ironTotal}`);
-    } */
   //EXPIERIMENTAL SWITCH STATEMENT
   switch (this.id) {
     case "foodBtn":
     if (foodTotal >= foodLimit) {
       return console.log(`Food limit of ${foodLimit} has been reached.`);
     } else {
+      //when specific resource is clicked then it is added to the total and updated on html page.
       foodTotal++;
+      $("#foodTotal").html(foodTotal);
       console.log(`Food: ${foodTotal}`);
     }
     break;
@@ -57,6 +36,7 @@ var ironLimit = 5;
       return console.log(`Wood limit of ${woodLimit} has been reached.`);
     } else {
       woodTotal++;
+      $("#woodTotal").html(woodTotal);
       console.log(`Wood: ${woodTotal}`);
     }
     break;
@@ -66,6 +46,7 @@ var ironLimit = 5;
       return console.log(`Stone limit of ${stoneLimit} has been reached.`);
     } else {
       stoneTotal++;
+      $("#stoneTotal").html(stoneTotal);
       console.log(`Stone: ${stoneTotal}`);
     }
     break;
@@ -75,6 +56,7 @@ var ironLimit = 5;
       return console.log(`Iron limit of ${ironLimit} has been reached.`);
     } else {
       ironTotal++;
+      $("#ironTotal").html(ironTotal);
       console.log(`Iron: ${ironTotal}`);
     }
     break;
@@ -85,9 +67,7 @@ var ironLimit = 5;
     
   })
 
-  //if resource total === resource limit then stop function
 
-  // else when partcular resource is click it is added the resource total
 
 
 
